8963d794366f57d38e84235a7c9876ced66c1d2a
Adam Lear <aalear@users.noreply.github.com>
2021-12-01 23:36:53 -0500

    Day 1

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,27 @@
+﻿var input = File.ReadAllLines("input.txt").Select(n => int.Parse(n)).ToList();
+
+var increasedCount = 0;
+int previous = int.MaxValue;
+foreach (var num in input)
+{
+    if (num > previous)
+    {
+        increasedCount++;
+    }
+    previous = num;
+}
+Console.WriteLine(increasedCount);
+
+previous = input.Take(3).Sum();
+increasedCount = 0;
+for (var i = 1; i < input.Count - input.Count % 3; i++)
+{
+    var sum = input[i] + input[i + 1] + input[i + 2];
+
+    if (sum > previous)
+    {
+        increasedCount++;
+    }
+    previous = sum;
+}
+Console.WriteLine(increasedCount);
